3b4935ea72c0276ab49e7c33739ea9cb7ad71b4e
Gregory Williams <grw300@gmail.com>
2017-04-30 11:51:49 -0500

    adding allowed hosts

--- riverDemo/settings.py
+++ riverDemo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'rl_@xpkc90(wf=f2h(xeji8=c1o@kx3jlp%_vnvn$p-_b=2o@8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [u'river-athena.herokuapp.com']
 
 
 # Application definition
